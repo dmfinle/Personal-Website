bfea42a2cd8051f01d6cbae7afcf4c420374a400
dmfinle <dfinley96@gmail.com>
2023-06-16 00:54:21 -0400

    Fixed white buffer on side on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
   });
 
   return (
-    <div className={`${theme}`}>
+    <div className={`${theme} overflow-hidden`}>
       <NavigationBar></NavigationBar>
       <DarkModeSwitch
         className="dark-toggle "
